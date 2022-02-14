5c371704915ee619e2ee43b01a17e5b2ba8d9039
ohheyitsjosh <78835111+ohheyitsjosh@users.noreply.github.com>
2022-02-13 20:45:46 -0800

    Keyboard changes

--- src/components/Keyboard.svelte
+++ src/components/Keyboard.svelte
@@ -2,10 +2,48 @@
      
 </script>
 
-<div>
-
+<div id="keyboard" class="flex flex-col w-screen">
+    <div id="key-row-1">
+        <button>Q</button>
+        <button>W</button>
+        <button>E</button>
+        <button>R</button>
+        <button>T</button>
+        <button>Y</button>
+        <button>U</button>
+        <button>I</button>
+        <button>O</button>
+        <button>P</button>
+    </div>
+    <div id="key-row-2">
+        <button>A</button>
+        <button>S</button>
+        <button>D</button>
+        <button>F</button>
+        <button>G</button>
+        <button>H</button>
+        <button>J</button>
+        <button>K</button>
+        <button>L</button>
+        <button>ENTER</button>
+    </div>
+    <div id="key-row-3">
+        <button>Z</button>
+        <button>X</button>
+        <button>C</button>
+        <button>V</button>
+        <button>B</button>
+        <button>N</button>
+        <button>M</button>
+    </div>
 </div>
 
 <style>
-
+    #keyboard button {
+        color: red;
+        border: 1px solid black;
+        padding: 15px;
+        border-radius: 25%;
+        width: 25px;
+    }
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,10 @@
+<script lang=ts>
+    import Keyboard from '../components/Keyboard.svelte';
+</script>
 <h1>Under construction.. :)</h1>
+<Keyboard />
+<style>
+
+</style>
+
+
