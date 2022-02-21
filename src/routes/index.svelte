a640ba26e9d58822dfbaaf7078055b6974186814
ohheyitsjosh <78835111+ohheyitsjosh@users.noreply.github.com>
2022-02-21 12:47:53 -0800

    Modal icons

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -18,10 +18,15 @@
 		gameOver,
 		stats
 	} from '../stores/gameStore';
-	import { browser } from '$app/env';
+	import Icon from '@iconify/svelte';
 
 	let guessCount = 0;
 	let newStats;
+	let showModal = false;
+
+	if ($gameOver) {
+		showModal = true; 
+	}
 
 	function updateArrays(event: any) {
 		const text = event.detail.text;
@@ -120,6 +125,7 @@
 				$gameOver = true;
 				updateStats();
 				saveGame();
+				showModal = true;
 				
 			} else {
 				$hasWon = false;
@@ -144,14 +150,22 @@
 			console.log('no navigator share support');
 		}
 	}
+
+	function toggleModal() {
+		showModal = !showModal;
+	}
 </script>
 
 <main class="h-screen w-screen bg-sunray">
 	<Header />
 	<Gameboard />
-	{#if $gameOver === true}
-		<div class="fixed items-center flex-col inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full">
-			<div class="mt-3 text-center bg-white w-1/2 md:w-1/3 py-8 rounded">
+	{#if $gameOver === true && showModal}
+		<div class="fixed flex items-center flex-col inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full">
+			<div class="flex flex-col mt-3 text-center bg-white w-1/2 md:w-1/3 py-8 rounded mt-20">
+				<button class="ml-auto px-4 align-middle" on:click="{toggleModal}">
+					<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" style="vertical-align: middle; display: inline;" width="20" height="20" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" d="M10.657 12.071L5 6.414L6.414 5l5.657 5.657L17.728 5l1.414 1.414l-5.657 5.657l5.657 5.657l-1.414 1.414l-5.657-5.657l-5.657 5.657L5 17.728z"/></svg>
+						Close					
+				</button>
 				<h3 class="text-lg font-medium text-gray-900">{ $hasWon ? 'Well done!' : 'Better luck next time.'}</h3>
 				<div class="mt-2 text-center">
 					<p class="text-md text-gray-500">{ $hasWon ? 'You did great.' : 'Tomorrow is all you.'}</p>
@@ -164,7 +178,9 @@
 							<li>Longest streak: {stats.maxDayStreak}</li>
 						</ul>
 					</div>
-					<button type="button" class="bg-frostbite p-2 rounded" on:click="{handleShare}">Share Results</button>
+					<button type="button" class="bg-frostbite p-2 rounded align-middle" on:click="{handleShare}"> 
+						<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" style="vertical-align: middle; display: inline;" width="20" height="20" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="M19.754 11a.75.75 0 0 1 .743.648l.007.102v7a3.25 3.25 0 0 1-3.065 3.246l-.185.005h-11a3.25 3.25 0 0 1-3.244-3.066l-.006-.184V11.75a.75.75 0 0 1 1.494-.102l.006.102v7a1.75 1.75 0 0 0 1.607 1.745l.143.006h11A1.75 1.75 0 0 0 19 18.894l.005-.143V11.75a.75.75 0 0 1 .75-.75ZM6.22 7.216l4.996-4.996a.75.75 0 0 1 .976-.073l.084.072l5.005 4.997a.75.75 0 0 1-.976 1.134l-.084-.073l-3.723-3.716l.001 11.694a.75.75 0 0 1-.648.743l-.102.007a.75.75 0 0 1-.743-.648L11 16.255V4.558L7.28 8.277a.75.75 0 0 1-.976.073l-.084-.073a.75.75 0 0 1-.073-.977l.073-.084l4.996-4.996L6.22 7.216Z"/></svg> Share Results
+					</button>
 				</div>				
 			</div>
 		</div>
