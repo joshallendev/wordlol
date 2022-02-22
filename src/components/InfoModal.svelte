b1220b12c2892f41bb37b15bb50ac1803c823e4c
ohheyitsjosh <78835111+ohheyitsjosh@users.noreply.github.com>
2022-02-21 19:46:29 -0800

    Fix modal height issue

--- src/components/InfoModal.svelte
+++ src/components/InfoModal.svelte
@@ -8,7 +8,7 @@
 
 
 <div in:fade out:fade class="fixed flex items-center flex-col inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full">
-    <div class="flex flex-col mt-3 text-center bg-white w-3/4 md:w-1/2 pt-2 pb-6 px-4 rounded mt-20">
+    <div class="flex flex-col mt-3 text-center bg-white w-3/4 md:w-1/2 pt-2 pb-6 px-4 rounded mt-20 h-3/4 overflow-y-auto">
         <button class="ml-auto align-middle" on:click="{() => $showInfo = !$showInfo}">
             <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" style="vertical-align: middle; display: inline;" width="25" height="25" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" d="M10.657 12.071L5 6.414L6.414 5l5.657 5.657L17.728 5l1.414 1.414l-5.657 5.657l5.657 5.657l-1.414 1.414l-5.657-5.657l-5.657 5.657L5 17.728z"/></svg>					
         </button>
