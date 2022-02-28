3afb7df1c29c06e2bb961fecd457d4404280032d
ohheyitsjosh <78835111+ohheyitsjosh@users.noreply.github.com>
2022-02-27 18:19:38 -0800

    Fix mobile header display issue

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,17 +3,17 @@
 </script>
 
 <header class="bg-black med:mb-10 text-white py-3 flex justify-between">
-	<div class="flex-row w-1/3 justify-start">
+	<div class="flex-row justify-start">
 		<p class="px-5 font-semibold text-left">WORDLOL</p>
 	</div>
 	{#if !$gameOver}
 		<div>
-			<button class="text-2xl w-1/3 {$hintsUsed < 2 ? 'animate-pulse' : ''}" type="button" on:click={() => $showHint = !$showHint}>
+			<button class="text-2xl {$hintsUsed < 2 ? 'animate-pulse' : ''}" type="button" on:click={() => $showHint = !$showHint}>
 				✨
 			</button>
 		</div>
 	{/if}
-	<div class="inline pr-5 w-1/3 justify-end text-right">
+	<div class="inline pr-5 justify-end text-right">
 		<button type="button" on:click={() => $showInfo = !$showInfo}>
 			<svg class="cursor-pointer" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" style="vertical-align: middle; display: inline;" width="30" height="30" preserveAspectRatio="xMidYMid meet" viewBox="0 0 20 20"><path fill="currentColor" d="M10.492 8.91A.5.5 0 0 0 9.5 9v4.502l.008.09a.5.5 0 0 0 .992-.09V9l-.008-.09Zm.307-2.16a.75.75 0 1 0-1.5 0a.75.75 0 0 0 1.5 0ZM18 10a8 8 0 1 0-16 0a8 8 0 0 0 16 0ZM3 10a7 7 0 1 1 14 0a7 7 0 0 1-14 0Z"/></svg>
 		</button>
